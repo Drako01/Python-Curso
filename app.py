58ab05e84becfdce807b0648df0c4a2a3f00cdde
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2022-02-21 15:37:09 -0300

    Curso Python

---
+++ app.py
@@ -0,0 +1,126 @@
+# def fun1(a):
+#     return None
+
+# def fun2(a):
+#     return fun1 (a)*fun1(a)
+# print(fun2(2))
+
+# def fun(a,b):
+#     return b**a
+# print(fun(b=2, 2))
+
+# z = 0
+# y = 10
+
+# x = y < z and z > y or y > z and z < y
+
+# print(x)
+
+# list = [x * x for x in range (5)]
+# def fun(lst):
+#     del lst[lst[2]]
+#     return lst
+# print(fun(list))
+    
+# x = 1
+# y = 2
+# x,y,z = x,x,y
+# z,y,z = x,y,z
+# print(x,y,z)
+
+
+# a = 1
+# b= 0
+
+# a=a^b
+# b=a^b
+# a=a^b
+# print(a,b)
+
+# def fun (x):
+#     if x % 2 == 0:
+#         return 1
+#     else:
+#         return 2
+    
+# print(fun(fun(2)))
+
+# nums = [1,2,3]
+# vals = nums
+# del vals[:]
+# print(vals)3
+
+# x = int(input())
+# y = int(input())
+# x=x%y
+# x=x%y
+# y= y%x
+# print(y)
+
+# y = input()
+# x= input()
+# print(x+y)
+
+# print ("a", "b", "c", sep="sep")
+
+# x=1 //5+1/5
+# print(x)
+
+# x= float(input())
+# y= float(input())
+# print(y**(1/x))
+
+# dct = {'uno':'dos', 'tres': 'uno', 'dos':'tres'}
+# v=dct['tres']
+
+# for k in range(len(dct)):
+#     v=dct[v]
+    
+# print(v)
+
+# lst = [i for i in range (-1,-2)]
+
+# print(lst)
+
+# def fun(a,b,c=0):
+    
+# def fun (x,y):
+#     if x == y:
+#         return x
+#     else:
+#         return fun (x,y-1)
+    
+# print(fun(0,3))
+
+# i = 0
+# while i < i +2 :
+#     i+=1
+#     print("*")
+# else:
+#     print("*")
+
+# dd={"1":"0","0":"1"}
+# for x in dd.vals():
+#     print(x, end="")
+
+# dct = {}
+# dct['1']=(1,2)
+# dct['2']=(2,1)
+# for x in dct.keys():
+#     print(dct[x][1], end="")
+
+
+# def fun(inp=2, out=3):
+#     return inp*out
+# print(fun(out=2))
+
+# lst = [[x for x in range (3)] for y in range (3)]
+
+# for r in range(3):
+#     for c in range(3):
+#         if lst[r][c]%2!=0:
+#             print("*")
+
+
+
+
